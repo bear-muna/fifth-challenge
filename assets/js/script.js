a6d8858e1084f0765b11dd873ca0af57a86daf36
bear-muna <bearmuna@gmail.com>
2023-04-06 10:02:29 -0700

    code for past present future

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+var timerEL = $('#currentDay');
+
+// var currentTimeHour = dayjs.utc();
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
@@ -20,9 +23,14 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-  $("#currentDay").text(dayjs().format("ddd, D MMMM YYYY"));
-});
 
+  var timeInterval = setInterval(function () {
+    
+    timerEL.text(dayjs().format("ddd, D MMMM YYYY hh:mm:ss"));
+    
+
+  }, 1000);
+});
 
 // jQuery to be used within entire assignment
     // $("") == query selector
@@ -47,3 +55,68 @@ $(function () {
 // Pulling saved events and render onto the according time block
   // Local storage saved within an array?
   // Create list elements then append onto the time block?
+
+// TODO: Past, Present, Future
+
+// function timeComparison() {
+//   // Should we make an array of all the cards?
+//   // Each card should have a signature id
+//   // Each id will indicate which hour it is
+
+//   dayjs().isBefore( , 'hour');
+
+//   dayjs().isSame( , 'hour');
+
+//   dayjs().isAfter( , 'hour');
+
+//   // Use for loop
+//   // Can use an if statement
+//   for (let i = 0; i < arr.length; i++) {
+
+
+//     if (... dayjs().isBefore(, 'hour')) {
+//       .. addClass('past');
+//     } else if (... daysjs().isAfter( , 'hour') {
+//       .. addClass('future');
+//     } else {
+//       .. addClass('present');
+//     }
+//   }
+
+
+//   // How to change id of the cards to dayjs()?
+// }
+var arr = [];
+
+var blockObj = {
+  time: undefined, 
+}
+
+arr.push(blockObj);
+
+var counter = 9;
+
+for (let i = 0; i < arr.length; i++) {
+  
+  arr[i].time = dayjs().hour(counter)
+
+  counter++;
+}
+
+console.log(arr);
+
+var test = {
+  time: dayjs().hour(12),
+}
+var timer = dayjs().hour();
+
+var res = dayjs().isBefore(test[0], 'hour');
+
+
+console.log(timer);
+
+console.log(res);
+
+console.log(test);
+
+console.log(dayjs());
